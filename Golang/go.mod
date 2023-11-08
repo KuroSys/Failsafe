3057501bd22ada65dcd2d4074faf4991e53af51b
KuroSys <102257683+KuroSys@users.noreply.github.com>
2023-11-08 20:40:19 +0100

    Add files via upload

---
+++ Golang/go.mod
@@ -0,0 +1,14 @@
+module gopls
+
+go 1.21.3
+
+require (
+	github.com/go-resty/resty/v2 v2.10.0
+	github.com/gookit/color v1.5.4
+)
+
+require (
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ Golang/handler.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/gookit/color"
+)
+
+type APIResponse struct {
+	Current float32 `json:"Current"`
+	Old     float32 `json:"Old"`
+}
+
+type MonitorHandler struct {
+	Monitored []string `json:"Monitored"`
+	Interval  int      `json:"Interval"`
+}
+
+const Vers = 1.1
+
+func checkup() {
+
+	apiURL := "https://kurosys.bio/api/check.json"
+
+	client := resty.New()
+
+	response, err := client.R().
+		Get(apiURL)
+
+	if err != nil {
+		log.Fatalf("[FATAL] Failed to Request API: %v", err)
+	}
+
+	var apiResponse APIResponse
+	if err := json.Unmarshal(response.Body(), &apiResponse); err != nil {
+		log.Fatalf("[FATAL] Failed to parase API: %v", err)
+	}
+
+	fmt.Println("Checking For Updates...")
+	if apiResponse.Current > Vers {
+		fmt.Println("[!] Found Newer version")
+		fmt.Println("[i] Downloading Newest..")
+
+		if err := os.Remove("handler"); err != nil {
+			fmt.Println("[ERROR] Failed To Delete Old File")
+		}
+
+		cmd := exec.Command("wget", "https://kurosys.bio/release/handler", "-q")
+		stdout, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println("[x] Failed To Download New File", err)
+			return
+		}
+		fmt.Println(string(stdout))
+		os.Exit(0)
+	} else if Vers == apiResponse.Current {
+		fmt.Println("[+] You are on the latest version")
+		time.Sleep(2 * time.Second)
+	} else if Vers > apiResponse.Current {
+		fmt.Println("You are on an unreleased Version? imao")
+	}
+	banner()
+}
+
+func banner() {
+	clearConsole()
+	banner := `
+	██╗  ██╗██╗   ██╗██████╗  ██████╗ 
+	██║ ██╔╝██║   ██║██╔══██╗██╔═══██╗
+	█████╔╝ ██║   ██║██████╔╝██║   ██║
+	██╔═██╗ ██║   ██║██╔══██╗██║   ██║
+	██║  ██╗╚██████╔╝██║  ██║╚██████╔╝
+	╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ 			
+		`
+	red := color.New(color.FgRed)
+	red.Println(banner)
+}
